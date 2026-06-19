ce22d8500680af7c6af0235003ce65be7c80d832
talel-ben-gharbia <talelbengharbia213@gmail.com>
2026-06-19 10:37:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+const PUBLIC = process.env.PUBLIC_URL;
+const tabs = Array.from({ length: 8 }, (_, i) => i + 1);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      backgroundImage: `url(${PUBLIC}/Images/background_vide.png)`
+    }}>
+      {tabs.map(i => (
+        <img
+          key={i}
+          src={`${PUBLIC}/Images/tab_${i}.png`}
+          alt={`tab ${i}`}
+          className="overlay"
+        />
+      ))}
+      <img src={`${PUBLIC}/Images/grand papier.png`} alt="grand papier" className="overlay" />
+      <img src={`${PUBLIC}/Images/petit papier.png`} alt="petit papier" className="overlay" />
+      <img src={`${PUBLIC}/Images/logo bhar.png`} alt="logo bhar" className="overlay" />
+      <img src={`${PUBLIC}/Images/logo isbat.png`} alt="logo isbat" className="overlay" />
     </div>
   );
 }
